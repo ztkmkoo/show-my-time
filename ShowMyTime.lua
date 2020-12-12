167d96a5b30454e93f2bf32d37c4a2a48533eac8
Kebron <ztkmkoo@gmail.com>
2020-12-13 03:54:50 +0900

    Feature/3 show my time on startup (#4)
    - show my buff remaining seconds when login and new aura accepted

--- ShowMyTime.lua
+++ ShowMyTime.lua
@@ -1,1 +1,29 @@
-message('HelloWorld')
+function PRINT_BUFF_REMAINING_SECONDS()
+    local maxIndex = 40
+    for i=1, maxIndex do
+        local name,_,_,_,_,expires = UnitAura("player", i)
+        
+        if name == nil then
+            break 
+        end
+
+        local currentTime = GetTime()
+        local remainingSeconds = expires - currentTime
+
+        if remainingSeconds > 0 then
+            print(name .. " remaining " .. remainingSeconds .. " seconds")
+        end
+    end
+end
+
+local addonName = "ShowMyTime"
+local onEvent = {}
+local frame = CreateFrame("Frame", addonName, nil)
+local customEvent = "UNIT_AURA"
+
+function onEvent.UNIT_AURA(event, ...)
+    PRINT_BUFF_REMAINING_SECONDS()
+end
+
+frame:RegisterUnitEvent(customEvent, "player")
+frame:SetScript("OnEvent", function(self, event, ...) onEvent[event](onEvent, ...) end)
